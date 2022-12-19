55b045b9072c8e05cb471b05a9c110f3443e551c
STamura2002 <s.tamura020107@gmail.com>
2022-12-20 02:32:35 +0900

    ブログ投稿の編集

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts', [PostController::class, 'store']);
+Route::post('posts', [PostController::class, 'store']);
+Route::get('/post/{post}/edit', [PostController::class, 'edit']);
