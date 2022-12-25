7bf39f521cfeaf74c86dcd6b28deff65c3f05c63
STamura2002 <s.tamura020107@gmail.com>
2022-12-25 14:17:07 +0900

    relation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 
 class PostController extends Controller
@@ -20,29 +21,22 @@ class PostController extends Controller
         return view('posts/show')->with(['post' => $post]);
     }
     
-    public function create()
+    public function edit(Post $post)
     {
-        return view('posts/create');
+        return view('posts/edit')->with(['post' => $post]);
     }
-    public function store(Post $post, PostRequest $request)
+
+    public function update(PostRequest $request, Post $post)
     {
-        $input = $request['post'];
-        $post->fill($input)->save();
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+    
         return redirect('/posts/' . $post->id);
     }
-    
-    public function edit(Post $post)
-{
-    return view('posts/edit')->with(['post' => $post]);
-}
 
-public function update(PostRequest $request, Post $post)
-{
-    $input_post = $request['post'];
-    $post->fill($input_post)->save();
-
-    return redirect('/posts/' . $post->id);
-}
-
+    public function create(Category $category)
+    {
+        return view('posts/create')->with(['categories' => $category->get()]);
+    }
 }
 ?>
